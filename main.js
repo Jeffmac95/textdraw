1a3bfebbd63afaad5cf5e009f056f24d12964133
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2023-08-09 15:56:15 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
-const brush_width = 10;
-const brush_height = 10;
 let brush_x = 0;
 let brush_y = 0;
 
-function draw_brush() {
-    // ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    ctx.beginPath();
-    ctx.rect(brush_x, brush_y, brush_width, brush_height);
-    ctx.fillStyle = "black";
-    ctx.fill();
+document.addEventListener('mousedown', start);
+document.addEventListener('mouseup', stop);
+
+function start(e) {
+    document.addEventListener('mousemove', draw);
+    pos(e);
 }
 
-function brush_pos(e) {
+function pos(e) {
     const rect = canvas.getBoundingClientRect();
-    mouse_x = e.clientX - rect.left;
-    mouse_y = e.clientY - rect.top;
-
-    if (mouse_x >= 0 && mouse_x < WIDTH &&
-        mouse_y >= 0 && mouse_y < HEIGHT) {
-        brush_x = mouse_x - brush_width / 2;
-        brush_y = mouse_y - brush_height / 2;
-    }
-
-    draw_brush();
+    brush_x = e.clientX - rect.left;
+    brush_y = e.clientY - rect.top;
 }
 
-canvas.addEventListener("mousedown", brush_pos);
-// canvas.addEventListener("mouseout", () => {
-//     ctx.clearRect(0, 0, WIDTH, HEIGHT)
-// });
+function stop() {
+    document.removeEventListener('mousemove', draw)
+}
+
+function draw(e) {
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(brush_x, brush_y);
+    pos(e);
+    ctx.lineTo(brush_x, brush_y);
+    ctx.stroke();
+    
+}
