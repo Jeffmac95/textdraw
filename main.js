4373b8b07fa20983703ebbd75b1d476ea8a1fc52
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2023-08-15 20:31:07 -0300

    added saveFile function

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const TEXT_AREA = document.querySelector("#txt");
+const FILE_NAME = document.querySelector("#fileName");
+const TXT_BUTTON = document.querySelector("#save_txt_button");
 
 let brush_x = 0;
 let brush_y = 0;
 
-document.addEventListener('mousedown', start);
-document.addEventListener('mouseup', stop);
+document.addEventListener("mousedown", start);
+document.addEventListener("mouseup", stop);
+TXT_BUTTON.addEventListener("click", saveFile);
 
 function start(e) {
-    document.addEventListener('mousemove', draw);
+    document.addEventListener("mousemove", draw);
     pos(e);
 }
 
@@ -19,17 +23,32 @@ function pos(e) {
 }
 
 function stop() {
-    document.removeEventListener('mousemove', draw)
+    document.removeEventListener("mousemove", draw)
 }
 
 function draw(e) {
     ctx.beginPath();
     ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = 'black';
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
     ctx.moveTo(brush_x, brush_y);
     pos(e);
     ctx.lineTo(brush_x, brush_y);
     ctx.stroke();
     
+}
+
+function saveFile() {
+    const textToSave = TEXT_AREA.value;
+    const blob = new Blob([textToSave], {
+        type: "text/plain"
+    });
+    const fileName = FILE_NAME.value + '.txt';
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = fileName;
+    downloadLink.click();
+
+    URL.revokeObjectURL(downloadLink.href);
 }
